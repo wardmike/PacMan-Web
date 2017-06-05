ec1527852a52d9623f08f5101d2b13c4c4ef754c
mikethebro <xenophon.of.athens@gmail.com>
2017-06-05 14:11:29 -0600

    adding startgame button

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function startGame()
 {
-
+    //myGameArea.start();
+    var canvas = document.getElementById("board");
+    canvas.style.backgroundColor = "#1b23b7";
 }
+
+/*var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function()
+    {
+        this.canvas.width = 1000;
+        this.canvas.id = "board";
+        this.canvas.height = 1000;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}*/
